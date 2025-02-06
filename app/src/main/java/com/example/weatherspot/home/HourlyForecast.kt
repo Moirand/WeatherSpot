70f2567c0c2b53e0dfcd1c5e11fbb9d133a468b2
Moirand <andreansyah1815@gmail.com>
2025-02-06 17:11:36 +0700

    fixing HourlyForecast.kt

--- app/src/main/java/com/example/weatherspot/home/HourlyForecast.kt
+++ app/src/main/java/com/example/weatherspot/home/HourlyForecast.kt
@@ -1,5 +1,6 @@
 package com.example.weatherspot.home
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyRow
@@ -7,20 +8,26 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.common.component.MyForecastCard
+import com.example.domain.model.HourItemDomain
+import com.example.weatherspot.utils.convertDoubleToIntOrDouble
+import java.time.LocalDateTime
 
+@SuppressLint("NewApi")
 @Preview(showBackground = true)
 @Composable
-fun HourlyForecast() {
+fun HourlyForecast(
+    listHourlyForecast: List<HourItemDomain?> = listOf()
+) {
     LazyRow(
         contentPadding = PaddingValues(16.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         items(24) { index ->
             MyForecastCard(
-                time = index + 1,
+                time = index,
                 icon = com.example.common.R.drawable.img_cloudy_windy_night,
-                temperature = 19,
-                isCurrent = true
+                temperature = convertDoubleToIntOrDouble(listHourlyForecast[index]!!.tempC!!).toString(),
+                isCurrent = index == LocalDateTime.now().hour
             )
         }
     }
--- app/src/main/java/com/example/weatherspot/utils/Utils.kt
+++ app/src/main/java/com/example/weatherspot/utils/Utils.kt
@@ -6,3 +6,11 @@ import androidx.core.content.ContextCompat
 
 fun hasPermission(context: Context, permission: String): Boolean =
     ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED
+
+fun convertDoubleToIntOrDouble(input: Double): Any {
+    return if (input % 1 == 0.0 || input.toString().substringAfter(".")[0] == '0') {
+        input.toInt()
+    } else {
+        input
+    }
+}
